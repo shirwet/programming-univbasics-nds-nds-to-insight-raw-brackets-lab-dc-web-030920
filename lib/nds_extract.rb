a55f1f4cf1aaf1f90b8296796c730f13425685a3
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-27 20:05:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,6 +30,6 @@ while movies_ar_index < nds.length do
     inner_index = 0
     while inner_index < inner_len do
       
-      directors_totals += nds[movies_ar_index]
+      directors_totals += nds[movies_ar_index][column_index][inner_index][:price]
 
 end
